ef7325e596c619b3fbfc6a43b67a13467539e05c
jainiljain31 <jainiljain31@gmail.com>
2024-11-29 21:18:47 +0530

    captain registration done

--- Backend/app.js
+++ Backend/app.js
@@ -6,7 +6,9 @@ const app = express();
 const cookieParser = require("cookie-parser")
 app.use(cors());
 const connectToDb = require("./db/db");
+
 const userRoutes = require("./routes/user.route");
+const captainRoutes = require("./routes/captain.routes")
 connectToDb();
 
 app.use(express.json());
@@ -18,5 +20,6 @@ app.get("/", (req, res) => {
 });
 
 app.use("/users", userRoutes);
+app.use("/captains",captainRoutes)
 
 module.exports = app;
--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -11,6 +11,12 @@ module.exports.registerUser = async (req, res, next) => {
 
   const { fullname, email, password } = req.body;
 
+  const isUserAlreadyExist = await userModel.findOne({ email: email });
+
+  if (isUserAlreadyExist) {
+    return res.status(400).json({ message: "User already exist" });
+  }
+
   const hashPassword = await userModel.hashPassword(password);
 
   const user = await userService.createUser({
@@ -53,7 +59,7 @@ module.exports.getUserProfile = async (req, res, next) => {
 
 module.exports.logoutUser = async (req, res, next) => {
   res.clearCookie("token");
-  const token = req.cookies.token || req.headers.authorization.split(' ')[1];
-  await blacklistTokenModel.create({token});
+  const token = req.cookies.token || req.headers.authorization.split(" ")[1];
+  await blacklistTokenModel.create({ token });
   res.status(200).json({ message: "Logged out successfully" });
 };
